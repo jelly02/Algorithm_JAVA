3b29319be258dd05e0678b82f4242b8bbbb3a5bc
jelly02 <haribo__@naver.com>
2021-11-28 23:54:27 +0900

    1_12 Password

---
+++ Algorithm/src/chap1/Password.java
@@ -0,0 +1,36 @@
+package chap1;
+
+import java.util.Scanner;
+
+public class Password {
+
+	public String solution(int n, String s){
+		
+		String answer="";
+		
+		for(int i=0; i<n; i++){
+			String tmp=s.substring(0, 7).replace('#', '1').replace('*', '0');
+			
+			int num=Integer.parseInt(tmp, 2);
+			
+			answer+=(char)num;
+			
+			s=s.substring(7);
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args){
+		
+		Password pd = new Password();
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int n=kb.nextInt();
+		
+		String str=kb.next();
+		
+		System.out.println(pd.solution(n, str));
+	}
+}
