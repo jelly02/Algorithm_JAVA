4d8b4b1e40dfe4f5323bc567160c14383bae97c6
jelly02 <haribo__@naver.com>
2021-12-06 18:34:43 +0900

    2_5 에라토스테네스의 체

---
+++ Algorithm/src/chap2/SieveOfEratosthenes.java
@@ -0,0 +1,32 @@
+package chap2;
+
+import java.util.Scanner;
+
+public class SieveOfEratosthenes {
+	
+	public int solution(int n){
+		int cnt=0;
+		
+		int[] ch = new int[n+1];
+		
+		for(int i=2; i<=n; i++){
+			if(ch[i]==0){
+				cnt++;
+				for(int j=i; j<=n; j=j+i) ch[j]=1;
+			}
+		}
+		
+		return cnt;
+	}
+	
+	public static void main(String[] args){
+		
+		SieveOfEratosthenes soe = new SieveOfEratosthenes();
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int n=kb.nextInt();
+		
+		System.out.println(soe.solution(n));
+	}
+}
