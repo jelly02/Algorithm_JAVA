beceb1ad1f3eb83d3afe3543a3b099269d488265
jelly02 <haribo__@naver.com>
2021-12-19 21:22:46 +0900

    3_4 연속 부분 수열

---
+++ Algorithm/src/chap3/ContinuousSubsequence.java
@@ -0,0 +1,40 @@
+package chap3;
+
+import java.util.Scanner;
+
+public class ContinuousSubsequence {
+
+	public int solution(int n, int m, int[] arr){
+		
+		int answer=0, sum=0, lt=0;
+		
+		for(int rt=0; rt<n; rt++){
+			sum+=arr[rt];
+			
+			if(sum==m) answer++;
+			
+			while(sum>=m){
+				sum-=arr[lt++];
+				if(sum==m) answer++; 
+			}
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args){
+		
+		ContinuousSubsequence T = new ContinuousSubsequence();
+		Scanner kb = new Scanner(System.in);
+		
+		int n=kb.nextInt();
+		int m=kb.nextInt();
+		int[] arr=new int[n];
+		
+		for(int i=0; i<n; i++){
+			arr[i]=kb.nextInt();
+		}
+		
+		System.out.print(T.solution(n, m, arr));
+	}
+}
