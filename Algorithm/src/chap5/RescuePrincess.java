eee80d14bc49ccd17ee9bc30b0d04b689d3e4db8
jelly02 <haribo__@naver.com>
2022-01-02 00:30:17 +0900

    5_6 공주 구하기

---
+++ Algorithm/src/chap5/RescuePrincess.java
@@ -0,0 +1,26 @@
+package chap5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class RescuePrincess {
+	public int solution(int n, int k){
+		int answer=0;
+		Queue<Integer> Q=new LinkedList<>();
+		for(int i=1; i<=n; i++) Q.offer(i);
+		while(!Q.isEmpty()){
+			for(int i=1; i<k; i++) Q.offer(Q.poll());
+			Q.poll();
+			if(Q.size()==1) answer=Q.poll();
+		}
+		return answer;
+	}
+	public static void main(String[] args){
+		RescuePrincess T = new RescuePrincess();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int k=kb.nextInt();
+		System.out.println(T.solution(n, k));
+	}
+}
