d48143bcdca51bbb0b748399d89dd98a16af8030
jelly02 <haribo__@naver.com>
2022-01-02 00:29:27 +0900

    5_5 쇠 막대기

---
+++ Algorithm/src/chap5/SteelStick.java
@@ -0,0 +1,26 @@
+package chap5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SteelStick {
+	public int solution(String str){
+		int cnt=0;
+		Stack<Character> stack=new Stack<>();
+		for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)=='(') stack.push('(');
+			else{
+				stack.pop();
+				if(str.charAt(i-1)=='(') cnt+=stack.size();
+				else cnt++;
+			}
+		}
+		return cnt;
+	}
+	public static void main(String[] args){
+		SteelStick T = new SteelStick();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.println(T.solution(str));
+	}
+}
