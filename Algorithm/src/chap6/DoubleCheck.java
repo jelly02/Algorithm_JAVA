bb14a109f9aaf503bcd3de3ae395f557db9fa400
jelly02 <haribo__@naver.com>
2022-01-13 14:06:22 +0900

    6_5 중복체크

---
+++ Algorithm/src/chap6/DoubleCheck.java
@@ -0,0 +1,27 @@
+package chap6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DoubleCheck {
+	public String solution(int n, int[] arr){
+		String answer="U";
+		Arrays.sort(arr);
+		for(int i=0; i<n-1; i++){
+			if(arr[i]==arr[i+1]){
+				answer="D";
+				break;
+			}
+		}
+		return answer;
+	}
+	public static void main(String[] args){
+		DoubleCheck T = new DoubleCheck();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+		System.out.println(T.solution(n, arr));
+	}
+
+}
