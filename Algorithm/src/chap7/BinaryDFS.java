eee7b3d9e0c96fff6823b93e62f006a70e7e6027
jelly02 <haribo__@naver.com>
2022-01-20 20:16:47 +0900

    7_2 이진수출력(재귀)

---
+++ Algorithm/src/chap7/BinaryDFS.java
@@ -0,0 +1,20 @@
+package chap7;
+
+public class BinaryDFS {
+	public void DFS(int n){
+		if(n==0) return;
+		else{
+			DFS(n/2);
+			System.out.print(n%2);
+		}
+	}
+
+	public void solution(int n){
+		DFS(n);
+	}
+	public static void main(String[] args){
+		BinaryDFS T = new BinaryDFS();
+		T.solution(11);
+		//System.out.println(T.solution(3));
+	}	
+}
